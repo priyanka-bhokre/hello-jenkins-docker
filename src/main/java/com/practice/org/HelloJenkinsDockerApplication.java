b98e59dbd2ea9b6e02b9d5fc899f5512bf7ecb3f
priyanka-bhokre <priyankabhokre6@gmail.com>
2021-03-09 23:20:29 +0530

    first commit

---
+++ src/main/java/com/practice/org/HelloJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/practice/org/api/HomeController.java
@@ -0,0 +1,15 @@
+package com.practice.org.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Welcome to jenkins";
+	}
+
+}
